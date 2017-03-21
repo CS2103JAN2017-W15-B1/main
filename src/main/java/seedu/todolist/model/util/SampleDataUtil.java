640bd11e164615cb470372e052ab4815a81d97bf
Shao Hui <shinlos@hotmail.com>
2017-03-22 03:13:31 +0800

    change SampleDataUtil.java

--- src/main/java/seedu/todolist/model/util/SampleDataUtil.java
+++ src/main/java/seedu/todolist/model/util/SampleDataUtil.java
@@ -4,7 +4,9 @@ import seedu.todolist.commons.exceptions.IllegalValueException;
 import seedu.todolist.model.ReadOnlyToDoList;
 import seedu.todolist.model.ToDoList;
 import seedu.todolist.model.tag.UniqueTagList;
+import seedu.todolist.model.task.EndTime;
 import seedu.todolist.model.task.Name;
+import seedu.todolist.model.task.StartTime;
 import seedu.todolist.model.task.Task;
 import seedu.todolist.model.task.UniqueTaskList.DuplicateTaskException;
 import seedu.todolist.model.task.parser.TaskParser;
@@ -13,12 +15,10 @@ public class SampleDataUtil {
     public static Task[] getSampleTasks() {
         try {
             return new Task[] {
-                TaskParser.parseTask(new Name("Alex Yeoh"), null, null, new UniqueTagList("friends")),
-                TaskParser.parseTask(new Name("Bernice Yu"), null, null, new UniqueTagList("colleagues", "friends")),
-                TaskParser.parseTask(new Name("Charlotte Oliveiro"), null, null, new UniqueTagList("neighbours")),
-                TaskParser.parseTask(new Name("David Li"), null, null, new UniqueTagList("family")),
-                TaskParser.parseTask(new Name("Irfan Ibrahim"), null, null, new UniqueTagList("classmates")),
-                TaskParser.parseTask(new Name("Roy Balakrishnan"), null, null, new UniqueTagList("colleagues"))
+                TaskParser.parseTask(new Name("Concert logistics"), new StartTime("11-11-2017 6.00 PM"), new EndTime("13-11-2017 6.00 PM"), new UniqueTagList("work", "logistics")),
+                TaskParser.parseTask(new Name("Reply email"), null, new EndTime("11-10-2017 6.00 PM"), new UniqueTagList("work", "highpriority")),
+                TaskParser.parseTask(new Name("clean up working table"), new StartTime("11-11-2016 6.00 PM"), new EndTime("12-11-2016 6.00 PM"), new UniqueTagList("chores", "lowpriority")),
+                TaskParser.parseTask(new Name("get present for billy"), new StartTime("24-12-2017 6.00 PM"), new EndTime("25-12-2017 6.00 PM"), new UniqueTagList("chores", "lowpriority"))
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
